b0c0fccb595d81582c7f3b69d4c5447afb19e0a3
Jake-py <jaras3950001@gmail.com>
2025-10-18 00:17:52 +0500

    Обновление фронтенда: новый index.js и правки конфигурации

--- front-end/vite.config.js
+++ front-end/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: '/Jibismcore/',
   define: { 'process.env': {} },
   esbuild: { legalComments: 'none' },
   build: { minify: true }
